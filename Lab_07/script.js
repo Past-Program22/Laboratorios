b0b4697087dc192501ca8e2babbab84ae3dad8a6
Carlos Daniel Arce Camacho <carcec@edu.uc.ac.cr>
2025-07-16 22:24:29 -0600

    16/7/25

--- Lab_07/script.js
+++ Lab_07/script.js
@@ -7,23 +7,22 @@ document.getElementById("validarBtn").addEventListener("click", function () {
     // Validación simple si los campos están vacíos
   if (nombre === "" || apellido === "") {
     mensaje.textContent = "Debe completar todos los campos.";
-    mensaje.style.color = "red";
+    mensaje.className = "alert alert-danger";
+
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Debes completar todos los campos!",
+        footer: '<a href="#">¿Por qué tengo este problema?</a>'
+    }); // Muestra un mensaje de alerta utilizando SweetAlert2
+
   } else {
     mensaje.textContent = `Bienvenido, ${nombre} ${apellido}`; // Actualiza el contenido del mensaje validación
     mensaje.className = "alert alert-success"; // Agrega una clase de Bootstrap para el estilo
-    mensaje.style.color = "black"; // Cambia el color del mensaje a negro
   } // Actualiza el contenido del mensaje validación si encuentra algo lo que sea
-  
-  Swal.fire({
-    icon: "error",
-    title: "Oops...",
-    text: "Debes completar todos los campos!",
-    footer: '<a href="#">¿Por qué tengo este problema?</a>'
-}); // Muestra un mensaje de alerta utilizando SweetAlert2
-
-  // alert(mensaje.textContent); // Muestra el mensaje en una alerta arriba del navegador
 
 });
+    // alert(mensaje.textContent); // Muestra el mensaje en una alerta arriba del navegador
 
 // jQuery: Agregar y quitar clase
 $("#agregarClase").click(function () {
@@ -39,15 +38,24 @@ $("#mostrarOcultar").click(function () {
   $("#elementoOculto").toggle();
 });
 
-function alternarTema(){
-    if (temaOscurso) {
-        document.getElementById("tema").className = "fa-solid fa-moon";
-    }
-    else {
-        document.getElementById("tema").className = "fa-solid fa-sun";
-    }
-    document.getElementById("main").classList.toggle("oscuro");
+let temaOscuro = false;
+
+function alternarTema() {
+  temaOscuro = temaOscuro ? false : true;
+  if (!temaOscuro) {
+    document.getElementById("tema").className = "fa-solid fa-moon";
+  }
+  else {
+    document.getElementById("tema").className = "fa-solid fa-sun";
+  }
+  document.getElementById("main").classList.toggle("oscuro");
 }
 
+// Contador dinámico
+let contador = 0;
+const contadorElemento = document.getElementById("contador");
 
-// Es recomendable utilizar ; al final de cada línea de código para evitar errores en JavaScript, aunque no es obligatorio en todos los casos.
+const modificarContador = (cambio) => {
+  contador += cambio;
+  contadorElemento.textContent = contador;
+};
